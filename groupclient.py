6fa24b7fcaba637c939edad6b88f3573b39d5286
Rishi Mehta <166524928+rishimehta04@users.noreply.github.com>
2024-04-24 20:13:19 +0530

    Update groupclient.py

--- groupclient.py
+++ groupclient.py
@@ -1,17 +1,29 @@
 import asyncio
 import websockets
-import keyboard
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.core.window import Window
 
-# start the websocket client
-async def start_client():
-    async with websockets.connect("ws://localhost:8765") as websocket:
-        done = False
-        while not done:
-            if keyboard.is_pressed("space"):
-                await websocket.send("buzz")
-                message = await websocket.recv()
-                print(message)
-                done = True
+class WebSocketClientApp(App):
+    async def start_client(self):
+        async with websockets.connect("ws://localhost:8765") as websocket:
+            done = False
+            while not done:
+                await asyncio.sleep(0.1)
+                if self.key_pressed == "space":
+                    await websocket.send("buzz")
+                    message = await websocket.recv()
+                    print(message)
+                    done = True
 
-# run the client
-asyncio.run(start_client())
+    def on_key_down(self, keyboard, keycode, text, modifiers):
+        self.key_pressed = text
+
+    def build(self):
+        self.key_pressed = None
+        Window.bind(on_key_down=self.on_key_down)
+        asyncio.ensure_future(self.start_client())
+        return Label(text="Press space to send 'buzz' message to the WebSocket server")
+
+if __name__ == '__main__':
+    WebSocketClientApp().run()
