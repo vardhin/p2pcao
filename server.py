2e1ade7e0bce99d366b01c1b7cda8d18e8a64ba4
Rishi Mehta <166524928+rishimehta04@users.noreply.github.com>
2024-04-27 17:48:09 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,12 +2,12 @@ from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 
 class Server(DatagramProtocol):
-    def __init__(self):
+    def __init__(self, host):
+        self.host = host
         self.clients = set()
 
     def startProtocol(self):
-        self.transport.getHost().host, self.transport.getHost().port
-        print(f"Server started on {self.transport.getHost().host}:{self.transport.getHost().port}")
+        print(f"Server started on {self.host}:{self.transport.getHost().port}")
 
     def datagramReceived(self, datagram, addr):
         datagram = datagram.decode('utf-8')
@@ -17,6 +17,7 @@ class Server(DatagramProtocol):
             self.clients.add(addr)
 
 if __name__ == '__main__':
-    reactor.listenUDP(0, Server())
+    server_host = input("Enter the server's IP address: ")
+    reactor.listenUDP(9999, Server(server_host))
     print("Server started")
     reactor.run()
